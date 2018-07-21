03fd4bab30b30ab40f9ffe4216d067e9df419a95
tarasryb <tarasryb@gmail.com>
2018-07-21 23:52:44 +0300

    Test updated

--- src/test/java/TestLibTest.java
+++ src/test/java/TestLibTest.java
@@ -1,4 +1,4 @@
-//import org.junit;
+import org.junit.jupiter.api.Assertions;
 
 class TestLibTest {
     @org.junit.jupiter.api.Test
@@ -6,7 +6,7 @@ class TestLibTest {
         TestLib testLib = new TestLib();
         Double testRes = testLib.test_calc(123.45, 678.91);
         Double origValue = 83811.4395;
-        // Assert.assertEquals(testRes, origValue);
+        Assertions.assertEquals(testRes, origValue);
     }
 //?
 }
